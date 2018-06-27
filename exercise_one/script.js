7aa053e97b0762eb3f7a5828f5134b901f205987
arenick <40619529+arenick@users.noreply.github.com>
2018-06-26 23:38:12 -0400

    exercise 1 files

---
+++ exercise_one/script.js
@@ -0,0 +1,14 @@
+function msg(num){
+  var total = 0
+  for (i=1; i<=num; i++){
+    total += i}
+    //document.write(i + "<br/>")}
+    return document.write(total)
+}
+
+
+
+//var total=0
+//for(var i=0; i<=5; i=+i){
+  //console.log(i);
+//}
